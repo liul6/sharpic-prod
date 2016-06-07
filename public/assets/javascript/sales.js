7ea20033b25533e9c0d7b51287399812211f0d6e
liul6 <yiqiu.liu@gmail.com>
2016-06-06 20:27:33 -0400

    Update sales.js

--- public/assets/javascript/sales.js
+++ public/assets/javascript/sales.js
@@ -272,6 +272,329 @@
         }
     };
 
+    $fileInput.find(':file').change(function() {
+        $auditsTable.hide();
+        $errorAlert.hide();
+        $successAlert.hide();
+        $activity.activity();
+        var file = this.files[0];
+        var type = "Valante";
+        
+        Papa.parse(file, {
+            complete: function (results) {
+                var validated = false;
+                var values = results.data;
+                var titles = ["PLU", "Menu Item", "Gross($) ", "#Voids", "Voids($)", "#Refunds", "Refunds($)", "Discounts($)", "Price Levels($)", "Options($)", "Quantity", "Net($)"];
+                if (values[0].length == 12) {
+                    validated = true;
+                    for (var i = 0; i < 12; i++) {
+                        if (values[0][i] != titles[i]) {
+                            validated = false;
+                            break;
+                        }
+                    }
+                }
+                if (!validated) {
+                    titles = ["Menu Item", "Quantity", "Sales", "Cost", "Percentage"];
+                    if (values[2].length == 5) {
+                        validated = true;
+                        for (var i = 0; i < 5; i++) {
+                            if (values[2][i] != titles[i]) {
+                                validated = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (!validated) {
+                      titles = ["Item Name", "Purchase Count", "Purchase Total", "Return Count", "Return Total", "Net Total"];
+                      if (values[2].length == 6) {
+                          validated = true;
+                          for (var i = 0; i < 6; i++) {
+                              if (values[2][i] != titles[i]) {
+                                  validated = false;
+                                  break;
+                              }
+                          }
+                      }
+                    }
+                    if (validated) {
+                        type = "TouchBistro";
+                    }
+                }
+
+                if (!validated) {
+                    titles = ["class_code", "desc_a", "code", "desc_b", "sum_qnty", "sum_price", "type"];
+                    if (values[0].length == 7) {
+                        validated = true;
+                        for (var i = 0; i < 7; i++) {
+                            if (values[0][i] != titles[i]) {
+                                validated = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (validated) {
+                        type = "SilverWare";
+                    }
+                }
+
+                if (!validated) {
+                    titles = ["Class", "Item", "Net Sales", "Disc", "Sales", "Qty"];
+                    if (values[0].length == 6) {
+                        validated = true;
+                        for (var i = 0; i < 6; i++) {
+                            if (values[0][i] != titles[i]) {
+                                validated = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (validated) {
+                        type = "SilverWare2";
+                    }
+                }
+
+                if (!validated) {
+                    titles = ["Rank", "Num", "Item Name", "Sold", "Sold", "Amount", "Cost", "Profit", "Cost %", "Sales"];
+                    if (values[0].length = titles.length) {
+                        validated = true;
+                        for (var i = 0; i < titles.length; i++) {
+                            if (values[0][i] != titles[i]) {
+                                validated = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (validated) {
+                        type = "Cibo";
+                    }
+                }
+                
+                var newRecipes = [];
+                
+                if (validated) {
+                    var Recipe = Parse.Object.extend('Recipe');
+                    var Sale = Parse.Object.extend('Sale');
+                    var query = new Parse.Query(Recipe);
+                    query.equalTo('client', client);
+                    query.equalTo('ignore', false);
+                    query.exists('recipeItems');
+                    query.descending('updatedAt');
+                    query.limit(1000);
+                    var newNonFoodRecipes=query.collection();
+                                        
+                    var foodrecipequery = new Parse.Query(Recipe);
+                    foodrecipequery.equalTo('client', client);
+                    foodrecipequery.equalTo('food', true);
+                    foodrecipequery.limit(1000);
+                    var newFoodRecipes=foodrecipequery.collection();
+                        
+                    Parse.Promise.when([newNonFoodRecipes.fetch(), newFoodRecipes.fetch()]).then(function(results) {
+                        newNonFoodRecipes.add(newFoodRecipes.models);
+                        newFoodRecipes.models = [];
+                        
+                        newNonFoodRecipes.forEach(function(oneRecipe) {
+                            newRecipes.push(oneRecipe);
+                        });
+                    });
+                    
+                    if(newRecipes.length<=0)
+                        validated = false;
+                }
+                
+                if(validated) {
+                    var salequery = new Parse.Query(Sale);
+                    salequery.equalTo('audit_id', audit.id);
+                    salequery.limit(1000);
+
+                    validated = false;
+                    salequery.find().then(function (oldsales) {
+                        Parse.Object.destroyAll(oldsales, { success: function(success) {
+                                    validated = true;
+                                },
+                                error: function(error) { 
+                                    validated = false;
+                                }
+                        });
+                    });
+                }
+                
+                var start = 1;
+                var end =0;
+                if(validated) {
+                    var recipes;
+                    if (type == "TouchBistro") {
+                        start = 3;
+                    }
+                    if (type == "TouchBistro") {
+                        end = values.length;
+                    }
+
+                    var recipesToSave = [];
+                    for (var i = start; i < end; i++) {
+                        var recipe = null;
+                        var name = "",
+                            amount = 0,
+                            gross = 0;
+                        if (type == "Valante") {
+                            name = values[i][1];
+                            amount = parseInt(values[i][10]);
+                            gross = parseFloat(values[i][2]);
+                        } else if (type == "TouchBistro") {
+                            name = values[i][0];
+                            amount = parseInt(values[i][1]);
+                            gross = parseFloat(values[i][2].replace(/[^0-9\.]+/g, ""));
+                        } else if (type == "SilverWare") {
+                            name = values[i][3];
+                            amount = values[i][4];
+                            gross = values[i][5];
+                        } else if (type == "SilverWare2") {
+                            name = values[i][1];
+                            amount = values[i][5];
+                            gross = values[i][4];
+                        } else if (type == "Cibo") {
+                            name = values[i][2];
+                            amount = parseInt(values[i][3]);
+                            gross = parseFloat(values[i][5].replace(/[^0-9\.]+/g, ""));
+                        }
+                        for (var j = 0; j < newRecipes.length; j++) {
+                            var aRecipe = newRecipes[j];
+                            if (aRecipe.get('name') == name) {
+                                recipe = aRecipe;
+                                if (!recipe.get('name')) {
+                                    recipe.set('name', name);
+                                }
+                                break;
+                            }
+                        }
+                        if (!recipe) {
+                            recipe = new Recipe({
+                                name: name,
+                                client: client,
+                                ignore: false,
+                                food: false
+                            });
+                            var acl = new Parse.ACL();
+                            acl.setRoleWriteAccess('Administrator', true);
+                            acl.setRoleReadAccess(client.get('name'), true);
+                            recipe.setACL(acl);
+                            recipesToSave.push(recipe);
+                        }                        
+                    }
+                    recipes = _.union(recipesToSave, newRecipes);
+                    
+                    var tempRecipes = [];
+                    var countRecipe =0;
+                    
+                    for ( var z = 0; z < recipesToSave.length; z++){
+                        countRecipe++;
+                        tempRecipes.push(recipesToSave[z]);
+                        if(countRecipe>5|| z==(recipesToSave.length-1)){
+                            Parse.Object.saveAll(tempRecipes, {
+                                success: function(objs) {
+                                    tempRecipes = [];
+                                    countRecipe=0;
+                                },
+                                error: function(error) { 
+                                    validated = false;
+                                    break;
+                                }
+                            });
+                        }                                                                     
+                    }
+                }
+                
+                if(validated) {
+                    for (var i = start; i < end; i++) {
+                        var recipe = null;
+                        var name = "",
+                            amount = 0,
+                            gross = 0;
+                        if (type == "Valante") {
+                            name = values[i][1];
+                            amount = parseInt(values[i][10]);
+                            gross = parseFloat(values[i][2]);
+                        } else if (type == "TouchBistro") {
+                            name = values[i][0];
+                            amount = parseInt(values[i][1]);
+                            gross = parseFloat(values[i][2].replace(/[^0-9\.]+/g,""));
+                        } else if (type == "SilverWare") {
+                            name = values[i][3];
+                            amount = parseInt(values[i][4]);
+                            gross = parseFloat(values[i][5]);
+                        } else if (type == "SilverWare2") {
+                            name = values[i][1];
+                            amount = parseInt(values[i][5]);
+                            gross = parseFloat(values[i][4]);
+                        } else if (type == "Cibo") {
+                            name = values[i][2];
+                            amount = parseInt(values[i][3]);
+                            gross = parseFloat(values[i][5].replace(/[^0-9\.]+/g, ""));
+                        }
+                        for (var j = 0; j < recipes.length; j++) {
+                            var aRecipe = recipes[j];
+                            if (aRecipe.get('name') == name) {
+                                recipe = aRecipe;
+                                if (!recipe.get('name')) {
+                                    recipe.set('name', name);
+                                }
+                                break;
+                            }
+                        }
+                        if (amount && recipe && !recipe.get('ignore')) {
+                            var sale = new Sale({
+                                recipe: recipe,
+                                amount: amount,
+                                price: parseFloat((gross / amount).toFixed(2))
+                            });
+                            var acl = new Parse.ACL();
+                            acl.setRoleWriteAccess('Administrator', true);
+                            acl.setRoleReadAccess(client.get('name'), true);
+                              sale.set("audit_id", audit.id);
+                            sale.setACL(acl);
+                            sales.push(sale);
+                        }
+                    }
+                    audit.set('sales', []);
+                    audit.save().then(function(audit) {
+                        var tempSales = [];
+                        var countSales = 0;
+                        var y = 0;
+                        
+                        for (y = 0; y < sales.length; y++) {
+                            countSales++;
+                            tempSales.push(sales[y]);
+                            if(countSales>5 || y==(sales.length-1)){
+                                Parse.Object.saveAll(tempSales, {
+                                    success: function(success) {
+                                        tempSales = [];
+                                        countSales=0;
+                                    },
+                                    error: function(error) { 
+                                        validated = false;
+                                        break;
+                                    }
+                                });
+                            }
+                        }                                                                     
+                    });
+                }
+                
+                if(validated){
+                    $auditsTable.show();
+                    $activity.activity(false);
+
+                    $successAlert.show();
+                }
+                else {
+                    $auditsTable.show();
+                    $activity.activity(false);
+                    $errorAlert.show();
+                }
+            }
+        });
+    });
+
     function hideAll() {
         $auditNotes.hide();
         $addAudit.hide();
