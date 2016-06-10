b88af755f9370c0664119d4aca0f7f6c1aa260c1
liul6 <yiqiu.liu@gmail.com>
2016-06-09 21:20:41 -0400

    Update sales.js

--- public/assets/javascript/sales.js
+++ public/assets/javascript/sales.js
@@ -136,7 +136,27 @@
             var model = this.model;
             if (model.get('recipe')) {
                 var recipeDescription = model.get('recipe').get('name');
-                this.$el.append();
+                if(!recipeDescription) {
+                    recipeDescription = "";
+                }
+                
+                recipeDescription = recipeDescription + "(";
+                    
+                if(model.get('recipe') && model.get('recipe').get('recipeItems') && model.get('recipe').get('recipeItems').length>0) {
+                    recipeDescription = recipeDescription + _.map(filteredArray, function(object) {
+                        if (object.get('product')) {
+                            var size = (object.get('product') && object.get('product').get('size') ? object.get('product').get('size').get('name') : "");
+                            var ounces = (object.get('ounces') ? ' ' + object.get('ounces') + ' ounces' : '');
+                            var fulls = (object.get('fulls') ? ' ' + object.get('fulls') + ' fulls' : '');
+                            return object.get('product').get('name') + ' ' + size + ounces + fulls;
+                        } else {
+                            return "";
+                        }
+                    }).join(", ");
+                }
+                recipeDescription = recipeDescription + ")";
+                
+                this.$el.append(recipeDescription);
             }
             this.delegateEvents();
             return this;
